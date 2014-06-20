2408b4dd8cf54e00bce22126f1393240b460e8d1
sorube13 <sorube@gmail.com>
2014-06-20 20:53:48 +0200

    v1.1

---
+++ Readme.md
@@ -0,0 +1,21 @@
+Done for Firefox OS
+=====================
+
+Done es una aplicación de gestión de tareas, hecha durante un hackatón para probar Firefox OS.
+
+Tecnologías empleadas
+---------------------
+
+- Firefox OS
+- Knockout.js
+- Gaia Building blocks
+- CSS3 transforms
+
+Capturas de pantalla
+---------------------
+![ScreenShot](https://raw.github.com/rlbisbe/done-firefoxos/master/snapshots/mainWindow.png)
+![ScreenShot](https://raw.github.com/rlbisbe/done-firefoxos/master/snapshots/newTask.png)
+
+En mi blog
+---------------------
+[Desarrollo para Firefox OS: Primer contacto](http://kcy.me/hcye)
---
+++ js/app.js
@@ -0,0 +1,53 @@
+var SimpleListModel = function(items) {
+
+    self = this;
+    self.items = ko.observableArray(items);
+    self.itemToAdd = ko.observable("");
+    self.count = ko.observable(items.length);
+    self.doneTasks = ko.observableArray([]);
+
+
+    self.addItem = function() {
+        if (self.itemToAdd() != "") {
+            var now1 = new Date();
+            var now = now1.getDate() + '/' + now1.getMonth() + '/' + now1.getFullYear();
+            self.items.push({title: self.itemToAdd(), added: now});
+            self.itemToAdd("");
+            self.count(items.length);
+            document.getElementById("list").dataset.pagePosition = "viewport";
+            document.getElementById("add-view").dataset.pagePosition = "right";
+            console.log("Se ha añadido correctamente");
+        }      
+    }.bind(this);
+
+    self.close = function(){
+        document.getElementById("list").dataset.pagePosition = "viewport";
+        document.getElementById("add-view").dataset.pagePosition = "right";
+
+    }
+
+    // this.checkTask = function(){
+    //     var titleT = $("#titleTask").val();
+    //     var dateT = $("#dateTask").val();
+
+    // }
+    this.removeTask = function (){
+        var y = self.items.indexOf(this);
+        self.items.splice(y,1);
+        console.log("ha funcionado");
+    }
+
+};
+ 
+ko.applyBindings(new SimpleListModel([
+        { title: "Probar Firefox OS", added: "10/01/2013" },
+        { title: "Escribir en el blog", added: "13/11/2012" },
+        { title: "Continuar el desarrollo   ", added: "12/09/2013" }
+    ]));
+
+    document.getElementById("add-btn").addEventListener("click", function(){
+        document.getElementById("list").dataset.pagePosition = "left";
+        document.getElementById("add-view").dataset.pagePosition = "viewport";
+    });
+
+
