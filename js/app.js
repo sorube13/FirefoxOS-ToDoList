bf794d823b15c90ceaf664d0ce9ca5c8c25207e9
sorube13 <sorube@gmail.com>
2014-06-21 21:09:12 +0200

    v2

--- js/app.js
+++ js/app.js
@@ -4,45 +4,79 @@ var SimpleListModel = function(items) {
     self.items = ko.observableArray(items);
     self.itemToAdd = ko.observable("");
     self.count = ko.observable(items.length);
-    self.doneTasks = ko.observableArray([]);
-
-
+    self.saveButton = ko.observable('Save New Task');
+    self.objectTitle = ko.observable('New Task');
+    self.indexItem = ko.observable();
+    //var flag2=true;
+    
     self.addItem = function() {
         if (self.itemToAdd() != "") {
-            var now1 = new Date();
-            var now = now1.getDate() + '/' + now1.getMonth() + '/' + now1.getFullYear();
-            self.items.push({title: self.itemToAdd(), added: now});
-            self.itemToAdd("");
-            self.count(items.length);
-            document.getElementById("list").dataset.pagePosition = "viewport";
-            document.getElementById("add-view").dataset.pagePosition = "right";
-            console.log("Se ha añadido correctamente");
-        }      
+           // if(flag2){
+           console.log("flag=true");
+           var now1 = new Date();
+           var now = now1.getDate() + '/' + now1.getMonth() + '/' + now1.getFullYear();
+           self.items.push({title: self.itemToAdd(), added: now});
+           self.itemToAdd("");
+           self.count(items.length);
+           document.getElementById("list").dataset.pagePosition = "viewport";
+           document.getElementById("add-view").dataset.pagePosition = "right";
+           console.log("Se ha añadido correctamente");
+           self.objectTitle('New Task');
+        }
+           /*if(!flag2){
+                console.log("flag=false");//editar
+                console.log(flag2);
+                var now1 = new Date();
+                var now = now1.getDate() + '/' + now1.getMonth() + '/' + now1.getFullYear();
+                self.items.splice(self.indexItem,1,{title: self.itemToAdd(), added: now});
+                document.getElementById("list").dataset.pagePosition = "viewport";
+                document.getElementById("add-view").dataset.pagePosition = "right";
+                console.log("Se ha editado correctamente");
+                self.itemToAdd("");
+                self.objectTitle('New Task');
+                self.saveButton('Save New Task');
+               // self.flag(true);
+            }*/
+            
+
     }.bind(this);
 
     self.close = function(){
+        self.objectTitle('New Task');
         document.getElementById("list").dataset.pagePosition = "viewport";
         document.getElementById("add-view").dataset.pagePosition = "right";
-
+       // self.flag(true);
+        self.saveButton('Save New Task');
+        self.itemToAdd("");
     }
 
     self.checkTask = function(){
-        console.log("checked!");
-   
+        //self.flag(false);
+        self.indexItem(self.items.indexOf(this));
+        self.objectTitle(this.title);
+        document.getElementById("list").dataset.pagePosition = "left";
+        document.getElementById("add-view").dataset.pagePosition = "viewport";
+        self.saveButton('Edit Task');
+        self.itemToAdd(this.title);
+           
     }
 
     self.removeTask = function (){
+        //self.flag(true);
         var y = self.items.indexOf(this);
         self.items.splice(y,1);
         console.log("ha funcionado");
+        self.saveButton('Save New Task');
+        
     }
+
 
 };
  
 ko.applyBindings(new SimpleListModel([
-        { title: "Probar Firefox OS", added: "10/01/2013" },
-        { title: "Escribir en el blog", added: "13/11/2012" },
-        { title: "Continuar el desarrollo   ", added: "12/09/2013" }
+        { title: "Obj1", added: "10/01/2014" },
+        { title: "Obj2", added: "13/11/2014" },
+        { title: "Obj3", added: "12/09/2014" }
     ]));
 
     document.getElementById("add-btn").addEventListener("click", function(){
